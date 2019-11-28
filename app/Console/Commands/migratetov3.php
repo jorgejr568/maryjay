d23d58a7c345e2ccb25244eb210043e5964cf083
Jorge Junior <jorge.junior.568@live.com>
2019-11-28 14:17:25 -0300

    tweets save only some data v3

---
+++ app/Console/Commands/migratetov3.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\GetDataFromTweet;
+use App\Tweet;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class MigrateToV3 extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'migrate:v3 {--per-page=1000}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Migrate application to v3';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+
+        $perPage = (int) $this->option('per-page');
+
+        $tweetsQuery = DB::table('tweets')->select(['id','data']);
+        $tweets = $tweetsQuery->paginate($perPage);
+
+
+        $lastPage = $tweets->lastPage();
+
+        $progress = $this->output->createProgressBar($tweets->total());
+
+
+        for($currentPage = 1;$currentPage <= $lastPage;$currentPage++) {
+            if($currentPage > 1){
+                $tweet = $tweetsQuery->simplePaginate($perPage,['*'],'page',$currentPage);
+            }
+            foreach ($tweets->items() as $tweet){
+                DB::table('tweets')->where('id',$tweet->id)->update([
+                    'data' => json_encode(Tweet::toV3(
+                        json_decode($tweet->data)
+                    ))
+                ]);
+
+                $progress->advance();
+            }
+            $tweets = null;
+        }
+        $progress->finish();
+    }
+}
--- app/Tweet.php
+++ app/Tweet.php
@@ -20,7 +20,39 @@ class Tweet extends Model
         'user_id',
         'data',
         'query',
-        'created_at',
-        'pre_data'
+        'created_at'
     ];
+
+    public static function toV3($tweet){
+        return [
+            "id" => $tweet->id,
+            "text" => $tweet->text,
+            "retweet_count" => $tweet->retweet_count,
+            "favorite_count" => $tweet->favorite_count,
+            "retweeted" => $tweet->retweeted,
+            "lang" => $tweet->lang,
+            "user" => [
+                "id" => $tweet->user->id,
+                "name" => $tweet->user->name,
+                "screen_name" => $tweet->user->screen_name,
+                "followers_count" => $tweet->user->followers_count,
+                "favourites_count" => $tweet->user->favourites_count,
+                "location" => $tweet->user->location
+            ],
+            "retweeted_status" => ($tweet->retweeted_status ?? false) ? [
+                "id" => $tweet->retweeted_status->id,
+                "text" => $tweet->retweeted_status->text,
+                "retweet_count" => $tweet->retweeted_status->retweet_count,
+                "favorite_count" => $tweet->retweeted_status->favorite_count,
+                "user" => [
+                    "id" => $tweet->retweeted_status->user->id,
+                    "name" => $tweet->retweeted_status->user->name,
+                    "screen_name" => $tweet->retweeted_status->user->screen_name,
+                    "followers_count" => $tweet->retweeted_status->user->followers_count,
+                    "location" => $tweet->retweeted_status->user->location,
+                    "favourites_count" => $tweet->retweeted_status->user->favourites_count
+                ]
+            ] : null,
+        ];
+    }
 }

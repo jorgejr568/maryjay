efc1460281ac4410d2f29aa4b548ffc2ba9b7389
Jorge Junior <jorge.junior.568@live.com>
2019-11-28 17:37:58 -0300

    migrate v3 optimized sql

--- app/Console/Commands/migratetov3.php
+++ app/Console/Commands/migratetov3.php
@@ -43,7 +43,7 @@ class MigrateToV3 extends Command
 
         $perPage = (int) $this->option('per-page');
 
-        $tweetsQuery = DB::table('tweets')->where('v3',false)->select(['id','data']);
+        $tweetsQuery = DB::table('tweets');//->where('v3',false);
         $tweets = $tweetsQuery->paginate($perPage);
 
 
@@ -51,23 +51,31 @@ class MigrateToV3 extends Command
 
         $progress = $this->output->createProgressBar($tweets->total());
 
-
         for($currentPage = 1;$currentPage <= $lastPage;$currentPage++) {
+            $insert = [];
+
             if($currentPage > 1){
                 $tweets = $tweetsQuery->simplePaginate($perPage,['*'],'page',$currentPage);
             }
+
             foreach ($tweets->items() as $tweet){
-                DB::table('tweets')->where('id',$tweet->id)->update([
-                    'data' => json_encode(Tweet::toV3(
+                $insert[$tweet->id] = (array) $tweet;
+
+                $insert[$tweet->id]['data'] = json_encode(
+                    Tweet::toV3(
                         json_decode($tweet->data)
-                    )),
-                    'v3' => true
-                ]);
+                    )
+                );
+                $insert[$tweet->id]['v3'] = true;
 
                 $progress->advance();
             }
             $tweets = null;
+
+            DB::table('tweets')->whereIn('id',array_keys($insert))->delete();
+            DB::table('tweets')->insert($insert);
         }
+
         $progress->finish();
     }
 }
